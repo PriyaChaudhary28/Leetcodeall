45085542cf9543ca76e2c78da72d996487b1d204
PriyaChaudhary28 <144237644+PriyaChaudhary28@users.noreply.github.com>
2024-12-12 21:18:51 +0530

    Time: 2 ms (61.64%), Space: 13.3 MB (16.26%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.cpp
@@ -6,17 +6,17 @@ public:
         for(int &i:gifts){//O(n)
              sum+=i;
         }
-        long long mysum=0;
+        
         while(k--){ //O(k)
            int maxe=pq.top();  //O(1)
             pq.pop(); //O(log(n))
             
             //floor taking
             int remaining=sqrt(maxe); //ignoring this time complexity
-            mysum+=maxe-remaining;
+            sum-=(maxe-remaining);
             pq.push(remaining); //O(log(n))
         }
-        return sum-mysum;
+        return sum;
         
         //k*logn total
     }
